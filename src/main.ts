a6f4a7e03885f2d315df370889512537a1b02954
emtiajium2 <ehasan@testreach.com>
2021-06-20 11:01:51 +0600

    added linter/formatter support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
-
+import { createApp } from 'vue';
 import { IonicVue } from '@ionic/vue';
+import App from './App.vue';
+import router from './router';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -23,10 +22,8 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+const app = createApp(App).use(IonicVue).use(router);
+
 router.isReady().then(() => {
-  app.mount('#app');
+    app.mount('#app');
 });
