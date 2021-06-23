e4b1baafbca392237c8ee91fef5bdcbca2bc6ed5
emtiajium <271emtiaj@gmail.com>
2021-06-23 12:41:13 +0600

    used generic view for multiple insert/remove

--- src/views/AddGenericNotes.vue
+++ src/views/AddGenericNotes.vue
@@ -2,70 +2,36 @@
     <ion-item lines="none">
         <ion-label>Generic Notes</ion-label>
     </ion-item>
-    <view v-for="(item, index) in itemsInGenericNotes" :key="index">
-        <ion-item>
-            <ion-input
-                type="text"
-                :value="genericNotes[index] || ''"
-                @keyup="insertGenericNote($event.target.value, index)"
-            ></ion-input>
-            <ion-button
-                v-show="index === itemsInGenericNotes.length - 1"
-                color="success"
-                @click="onAddMoreGenericNote"
-                >{{ getAddButtonLabel(genericNotes.length) }}</ion-button
-            >
-            <ion-button
-                v-show="index === itemsInGenericNotes.length - 1"
-                color="danger"
-                @click="onRemoveLastGenericNote(index)"
-                >Remove</ion-button
-            >
-        </ion-item>
-    </view>
+    <add-remove-multiple-items ref="AddRemoveMultipleItemsRef" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonInput, IonItem, IonLabel, IonButton } from '@ionic/vue';
-import { v4 as uuidV4 } from 'uuid';
+import { IonItem, IonLabel } from '@ionic/vue';
+import AddRemoveMultipleItems from '@/views/AddRemoveMultipleItems.vue';
 
 export default defineComponent({
     name: 'AddGenericNotes',
     components: {
-        IonInput,
+        AddRemoveMultipleItems,
         IonLabel,
         IonItem,
-        IonButton,
     },
     data() {
         return {
-            itemsInGenericNotes: [uuidV4()],
             genericNotes: [] as string[],
         };
     },
     methods: {
-        getAddButtonLabel(itemsLength: number) {
-            return itemsLength ? 'Add More' : 'Add';
-        },
-        insertGenericNote(genericNote: string, index: number) {
-            this.genericNotes[index] = genericNote.trim();
-        },
-        onAddMoreGenericNote() {
-            this.itemsInGenericNotes.push(uuidV4());
-        },
-        onRemoveLastGenericNote(removableIndex: number) {
-            if (removableIndex !== 0) {
-                this.itemsInGenericNotes.pop();
-            }
-            this.genericNotes = this.genericNotes.filter((genericNote, index) => index !== removableIndex);
-        },
         getGenericNotes() {
-            return this.genericNotes.filter((genericNote) => genericNote.length > 0);
+            this.genericNotes = (
+                this.$refs.AddRemoveMultipleItemsRef as InstanceType<typeof AddRemoveMultipleItems>
+            ).getItems();
+            return this.genericNotes;
         },
         clear() {
+            (this.$refs.AddRemoveMultipleItemsRef as InstanceType<typeof AddRemoveMultipleItems>).clear();
             this.genericNotes = [];
-            this.itemsInGenericNotes = [uuidV4()];
         },
     },
 });
