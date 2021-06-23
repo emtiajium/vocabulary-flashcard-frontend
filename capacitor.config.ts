7d0acbc8433d094992e3cbf1e4da99ad970db216
emtiajium <271emtiaj@gmail.com>
2021-06-23 12:04:40 +0600

    capacitor.config.json > capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.emtiajium.ielts.gibberish.android',
+    appName: 'IELTS Gibberish',
+    webDir: 'dist',
+    bundledWebRuntime: false,
+};
+
+export default config;
