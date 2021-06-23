78d49ef715de59683e3c5a5c5584e76b01d19182
emtiajium <271emtiaj@gmail.com>
2021-06-23 12:37:49 +0600

    used generic view for multiple insert/remove

--- src/views/AddLinkerWords.vue
+++ src/views/AddLinkerWords.vue
@@ -2,67 +2,36 @@
     <ion-item lines="none">
         <ion-label>Linker Words</ion-label>
     </ion-item>
-    <view v-for="(item, index) in itemsInLinkerWords" :key="index">
-        <ion-item>
-            <ion-input
-                type="text"
-                :value="linkerWords[index] || ''"
-                @keyup="insertLinkerWord($event.target.value, index)"
-            ></ion-input>
-            <ion-button v-show="index === itemsInLinkerWords.length - 1" color="success" @click="onAddMoreLinkerWord">{{
-                getAddButtonLabel(linkerWords.length)
-            }}</ion-button>
-            <ion-button
-                v-show="index === itemsInLinkerWords.length - 1"
-                color="danger"
-                @click="onRemoveLastLinkerWord(index)"
-                >Remove</ion-button
-            >
-        </ion-item>
-    </view>
+    <add-remove-multiple-items ref="AddRemoveMultipleItemsRef" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonInput, IonItem, IonLabel, IonButton } from '@ionic/vue';
-import { v4 as uuidV4 } from 'uuid';
+import { IonItem, IonLabel } from '@ionic/vue';
+import AddRemoveMultipleItems from '@/views/AddRemoveMultipleItems.vue';
 
 export default defineComponent({
     name: 'AddLinkerWords',
     components: {
-        IonInput,
+        AddRemoveMultipleItems,
         IonLabel,
         IonItem,
-        IonButton,
     },
     data() {
         return {
-            itemsInLinkerWords: [uuidV4()],
             linkerWords: [] as string[],
         };
     },
     methods: {
-        getAddButtonLabel(itemsLength: number) {
-            return itemsLength ? 'Add More' : 'Add';
-        },
-        insertLinkerWord(linkerWord: string, index: number) {
-            this.linkerWords[index] = linkerWord.trim();
-        },
-        onAddMoreLinkerWord() {
-            this.itemsInLinkerWords.push(uuidV4());
-        },
-        onRemoveLastLinkerWord(removableIndex: number) {
-            if (removableIndex !== 0) {
-                this.itemsInLinkerWords.pop();
-            }
-            this.linkerWords = this.linkerWords.filter((linkerWord, index) => index !== removableIndex);
-        },
         getLinkerWords() {
-            return this.linkerWords.filter((linkerWord) => linkerWord.length > 0);
+            this.linkerWords = (
+                this.$refs.AddRemoveMultipleItemsRef as InstanceType<typeof AddRemoveMultipleItems>
+            ).getItems();
+            return this.linkerWords;
         },
         clear() {
+            (this.$refs.AddRemoveMultipleItemsRef as InstanceType<typeof AddRemoveMultipleItems>).clear();
             this.linkerWords = [];
-            this.itemsInLinkerWords = [uuidV4()];
         },
     },
 });
---
+++ src/views/AddRemoveMultipleItems.vue
@@ -0,0 +1,74 @@
+<template>
+    <view v-for="(item, index) in placeholderItems" :key="index">
+        <ion-item>
+            <ion-input
+                type="text"
+                :value="items[index] || ''"
+                @keyup="insertItem($event.target.value, index)"
+            ></ion-input>
+            <ion-button
+                v-show="items.length > 0 && index === placeholderItems.length - 1"
+                color="success"
+                @click="onAddMoreItem"
+                >Add More</ion-button
+            >
+            <ion-button
+                v-show="items.length > 0 && index === placeholderItems.length - 1"
+                color="danger"
+                @click="onRemoveLastItem(index)"
+                >Remove</ion-button
+            >
+        </ion-item>
+    </view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonInput, IonItem, IonButton } from '@ionic/vue';
+import { v4 as uuidV4 } from 'uuid';
+
+export default defineComponent({
+    name: 'AddRemoveMultipleItems',
+    components: {
+        IonInput,
+        IonItem,
+        IonButton,
+    },
+    data() {
+        return {
+            placeholderItems: [uuidV4()],
+            items: [] as string[],
+        };
+    },
+    methods: {
+        insertItem(item: string, index: number) {
+            if (!item) {
+                this.removeItem(index);
+            } else {
+                this.items[index] = item.trim();
+            }
+        },
+        onAddMoreItem() {
+            this.placeholderItems.push(uuidV4());
+        },
+        removeItem(removableIndex: number) {
+            this.items = this.items.filter((word, index) => index !== removableIndex);
+        },
+        onRemoveLastItem(removableIndex: number) {
+            if (removableIndex !== 0) {
+                this.placeholderItems.pop();
+            }
+            this.removeItem(removableIndex);
+        },
+        getItems() {
+            return this.items.filter((item) => item.length > 0);
+        },
+        clear() {
+            this.items = [];
+            this.placeholderItems = [uuidV4()];
+        },
+    },
+});
+</script>
+
+<style scoped></style>
