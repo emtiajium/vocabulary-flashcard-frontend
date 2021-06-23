0e2ed7606651e0012424d8fd3988492b68f6332f
emtiajium <271emtiaj@gmail.com>
2021-06-23 12:47:44 +0600

    used generic view for multiple insert/remove

--- src/views/AddDefinitionExternalLinks.vue
+++ src/views/AddDefinitionExternalLinks.vue
@@ -2,70 +2,34 @@
     <ion-item lines="none">
         <ion-label>External Links</ion-label>
     </ion-item>
-    <view v-for="(item, index) in itemsInExternalLinks" :key="index">
-        <ion-item>
-            <ion-input
-                type="url"
-                :value="externalLinks[index] || ''"
-                @keyup="insertExternalLink($event.target.value, index)"
-            ></ion-input>
-            <ion-button
-                v-show="index === itemsInExternalLinks.length - 1"
-                color="success"
-                @click="onAddMoreExternalLink"
-                >{{ getAddButtonLabel(externalLinks.length) }}</ion-button
-            >
-            <ion-button
-                v-show="index === itemsInExternalLinks.length - 1"
-                color="danger"
-                @click="onRemoveLastExternalLink(index)"
-                >Remove</ion-button
-            >
-        </ion-item>
-    </view>
+    <add-remove-multiple-items ref="AddRemoveMultipleItemsRef" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonInput, IonItem, IonLabel, IonButton } from '@ionic/vue';
-import { v4 as uuidV4 } from 'uuid';
+import { IonItem, IonLabel } from '@ionic/vue';
+import AddRemoveMultipleItems from '@/views/AddRemoveMultipleItems.vue';
 
 export default defineComponent({
     name: 'AddDefinitionExternalLinks',
     components: {
-        IonInput,
+        AddRemoveMultipleItems,
         IonLabel,
         IonItem,
-        IonButton,
     },
     data() {
         return {
-            itemsInExternalLinks: [uuidV4()],
             externalLinks: [] as string[],
         };
     },
     methods: {
-        getAddButtonLabel(itemsLength: number) {
-            return itemsLength ? 'Add More' : 'Add';
-        },
-        insertExternalLink(externalLink: string, index: number) {
-            this.externalLinks[index] = externalLink.trim();
-        },
-        onAddMoreExternalLink() {
-            this.itemsInExternalLinks.push(uuidV4());
-        },
-        onRemoveLastExternalLink(removableIndex: number) {
-            if (removableIndex !== 0) {
-                this.itemsInExternalLinks.pop();
-            }
-            this.externalLinks = this.externalLinks.filter((externalLink, index) => index !== removableIndex);
-        },
         getExternalLinks() {
-            return this.externalLinks.filter((externalLink) => externalLink.length > 0);
+            this.externalLinks = this.$refs.AddRemoveMultipleItemsRef as InstanceType<typeof AddRemoveMultipleItems>;
+            return this.externalLinks;
         },
         clear() {
+            (this.$refs.AddRemoveMultipleItemsRef as InstanceType<typeof AddRemoveMultipleItems>).clear();
             this.externalLinks = [];
-            this.itemsInExternalLinks = [uuidV4()];
         },
     },
 });
