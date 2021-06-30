1fc8fa838f7c55e4931b501bd671efa0b24c7c66
emtiajium <271emtiaj@gmail.com>
2021-06-30 21:42:50 +0600

    basic setup

---
+++ src/views/Vocabulary.vue
@@ -0,0 +1,38 @@
+<template>
+    <ion-page>
+        <ion-header :translucent="true">
+            <ion-toolbar>
+                <ion-title>Vocabulary</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content :fullscreen="true">
+            <ion-card>
+                <ion-card-header>
+                    <ion-card-title>Hello!</ion-card-title>
+                </ion-card-header>
+            </ion-card>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonToolbar, IonHeader, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle } from '@ionic/vue';
+
+export default defineComponent({
+    name: 'Vocabulary',
+    components: {
+        IonPage,
+        IonToolbar,
+        IonHeader,
+        IonTitle,
+        IonContent,
+        IonCard,
+        IonCardHeader,
+        IonCardTitle,
+    },
+});
+</script>
+
+<style scoped></style>
