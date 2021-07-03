8a0884a0b313f00d8b98671cc064f4b6937f0096
emtiajium <271emtiaj@gmail.com>
2021-07-03 16:00:31 +0600

    displayed profile info

--- src/views/FirecrackerMenu.vue
+++ src/views/FirecrackerMenu.vue
@@ -1,11 +1,15 @@
 <template>
     <ion-menu side="start" :menu-id="menuId" :content-id="contentId">
         <ion-content>
+            <div class="profile ion-padding-bottom">
+                <img :src="profilePictureUrl" alt="Avatar" class="rounded-circle ion-padding" width="100" />
+                <div class="ion-padding-start">
+                    <strong class="item-text">{{ fullName }}</strong>
+                    <br />
+                    <span class="ion-padding-top item-text">{{ username }}</span>
+                </div>
+            </div>
             <ion-list>
-                <ion-item>Menu Item</ion-item>
-                <ion-item>Menu Item</ion-item>
-                <ion-item>Menu Item</ion-item>
-                <ion-item>Menu Item</ion-item>
                 <ion-item>Menu Item</ion-item>
             </ion-list>
         </ion-content>
@@ -15,6 +19,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonContent, IonItem, IonList, IonMenu, menuController } from '@ionic/vue';
+import NativeStorage from '@/utils/NativeStorage';
 
 export default defineComponent({
     name: 'FirecrackerMenu',
@@ -25,6 +30,19 @@ export default defineComponent({
         IonMenu,
     },
     props: ['contentId', 'menuId'],
+    data() {
+        return {
+            profilePictureUrl: '',
+            username: '',
+            fullName: '',
+        };
+    },
+    async mounted() {
+        const user = await NativeStorage.getAuthorizedUser();
+        this.profilePictureUrl = user.profilePictureUrl as string;
+        this.username = user.username as string;
+        this.fullName = `${user.firstname?.trim()} ${(user.lastname || '').trim()}`.trim();
+    },
     methods: {
         toggleMenu() {
             menuController.enable(true, this.menuId);
@@ -34,4 +52,14 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.profile {
+    background-color: #2e8b57;
+}
+.rounded-circle {
+    border-radius: 50%;
+}
+.item-text {
+    color: #ffffff;
+}
+</style>
