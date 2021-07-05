86932db4e897d4909f2d570db3702c0784f92532
emtiajium <271emtiaj@gmail.com>
2021-07-05 20:49:36 +0600

    replaced more files

--- replace-generated-android-files.sh
+++ replace-generated-android-files.sh
@@ -2,17 +2,18 @@
 cp ./android-replacer/AndroidManifest.xml ./android/app/src/main/AndroidManifest.xml
 cp ./android-replacer/ic_launcher_background.xml ./android/app/src/main/res/values/ic_launcher_background.xml
 cp ./android-replacer/strings.xml ./android/app/src/main/res/values/strings.xml
+cp ./android-replacer/styles.xml ./android/app/src/main/res/values/styles.xml
 cp ./android-replacer/variables.gradle ./android/variables.gradle
+rm -rf ./android/app/src/main/res/values-v26 && mkdir ./android/app/src/main/res/values-v26
+cp ./android-replacer/styles-v26.xml ./android/app/src/main/res/values-v26/styles.xml
 # Splash screen
 cp -R ./android-replacer/drawable ./android/app/src/main/res
 cp -R ./android-replacer/drawable-land-hdpi ./android/app/src/main/res
-# cp -R ./android-replacer/drawable-land-ldpi ./android/app/src/main/res
 cp -R ./android-replacer/drawable-land-mdpi ./android/app/src/main/res
 cp -R ./android-replacer/drawable-land-xhdpi ./android/app/src/main/res
 cp -R ./android-replacer/drawable-land-xxhdpi ./android/app/src/main/res
 cp -R ./android-replacer/drawable-land-xxxhdpi ./android/app/src/main/res
 cp -R ./android-replacer/drawable-port-hdpi ./android/app/src/main/res
-# cp -R ./android-replacer/drawable-port-ldpi ./android/app/src/main/res
 cp -R ./android-replacer/drawable-port-mdpi ./android/app/src/main/res
 cp -R ./android-replacer/drawable-port-xhdpi ./android/app/src/main/res
 cp -R ./android-replacer/drawable-port-xxhdpi ./android/app/src/main/res
