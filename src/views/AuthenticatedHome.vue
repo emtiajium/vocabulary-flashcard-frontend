f8a6af4940a8603306459b49fad874990d1e803f
emtiajium <271emtiaj@gmail.com>
2021-07-05 21:19:40 +0600

    handled back-button

--- src/views/AuthenticatedHome.vue
+++ src/views/AuthenticatedHome.vue
@@ -5,11 +5,21 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import VocabularyList from '@/views/VocabularyList.vue';
+import { useBackButton, useIonRouter } from '@ionic/vue';
+import { App } from '@capacitor/app';
 
 export default defineComponent({
     name: 'AuthenticatedHome',
     components: {
         VocabularyList,
+    },
+    setup() {
+        const ionRouter = useIonRouter();
+        useBackButton(-1, () => {
+            if (!ionRouter.canGoBack()) {
+                App.exitApp();
+            }
+        });
     },
 });
 </script>
