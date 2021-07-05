ac9008d1c4d7ad0854b322f699fa848597a79bc5
emtiajium <271emtiaj@gmail.com>
2021-07-05 21:27:52 +0600

    moved back-button functionality to the home view

--- src/views/AuthenticatedHome.vue
+++ src/views/AuthenticatedHome.vue
@@ -5,21 +5,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import VocabularyList from '@/views/VocabularyList.vue';
-import { useBackButton, useIonRouter } from '@ionic/vue';
-import { App } from '@capacitor/app';
 
 export default defineComponent({
     name: 'AuthenticatedHome',
     components: {
         VocabularyList,
-    },
-    setup() {
-        const ionRouter = useIonRouter();
-        useBackButton(-1, () => {
-            if (!ionRouter.canGoBack()) {
-                App.exitApp();
-            }
-        });
     },
 });
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,21 @@
 import { defineComponent } from 'vue';
 import Spinner from '@/views/Spinner.vue';
 import NativeStorage from '@/utils/NativeStorage';
+import { useBackButton, useIonRouter } from '@ionic/vue';
+import { App } from '@capacitor/app';
 
 export default defineComponent({
     name: 'Home',
     components: {
         Spinner,
+    },
+    setup() {
+        const ionRouter = useIonRouter();
+        useBackButton(-1, () => {
+            if (!ionRouter.canGoBack()) {
+                App.exitApp();
+            }
+        });
     },
     async mounted() {
         const user = await NativeStorage.getAuthorizedUser();
