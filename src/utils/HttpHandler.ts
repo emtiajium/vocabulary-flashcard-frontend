966e3efd3fd093630582c5f15b7d4b3afc06adf4
emtiajium <271emtiaj@gmail.com>
2021-07-06 21:47:37 +0600

    passed X-User-Id

--- src/utils/HttpHandler.ts
+++ src/utils/HttpHandler.ts
@@ -2,19 +2,29 @@ import AxiosAdapter from '@/utils/AxiosAdapter';
 import NativeStorage from '@/utils/NativeStorage';
 import Config from '../../config.json';
 
+type RequestConfig = Record<string, unknown>;
+
 export default class HttpHandler {
-    static async getRequestConfig(): Promise<Record<string, unknown>> {
-        return {
-            headers: { 'content-type': 'application/json', 'X-User-Id': await NativeStorage.getUserId() },
+    static async getRequestConfig(isPublic = false): Promise<RequestConfig> {
+        const requestConfig: RequestConfig = {
+            'content-type': 'application/json',
         };
+        if (!isPublic) {
+            requestConfig['X-User-Id'] = await NativeStorage.getUserId();
+        }
+        return { headers: requestConfig };
     }
 
     static getCompleteUrl(url: string): string {
         return `${Config.server.apiPrefix}${url}`;
     }
 
-    static async post<TPayload, UResponse>(url: string, payload: TPayload): Promise<UResponse> {
-        return AxiosAdapter.post(HttpHandler.getCompleteUrl(url), payload, await HttpHandler.getRequestConfig());
+    static async post<TPayload, UResponse>(url: string, payload: TPayload, isPublic = false): Promise<UResponse> {
+        return AxiosAdapter.post(
+            HttpHandler.getCompleteUrl(url),
+            payload,
+            await HttpHandler.getRequestConfig(isPublic),
+        );
     }
 
     static async get<TResponse>(url: string): Promise<TResponse> {
