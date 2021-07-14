af20f3de8fcbe795e3901d3d77fdf87728f3c827
emtiajium <271emtiaj@gmail.com>
2021-07-14 12:19:04 +0600

    moved the material icon import from the public file

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import '@/theme/variables.css';
 
+import '@/theme/material-icons.css';
 import '@/theme/custom-styles.css';
 
 const app = createApp(App).use(IonicVue).use(router);
