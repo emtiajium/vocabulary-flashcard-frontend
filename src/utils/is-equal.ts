eb06ac339ef77bde0f3af1e2f0e39f1faf971f49
emtiajium <271emtiaj@gmail.com>
2021-07-14 22:56:48 +0600

    equality checker

---
+++ src/utils/is-equal.ts
@@ -0,0 +1,13 @@
+import * as _ from 'lodash';
+
+export function isArrayOfStringEqual(firstArray: string[], secondArray: string[]): boolean {
+    return firstArray.length === secondArray.length ? _.difference(firstArray, secondArray).length > 0 : true;
+}
+
+export function isArrayOfObjectEqual<T>(firstPOJO: T[], secondPOJO: T[]): boolean {
+    return firstPOJO.length === secondPOJO.length
+        ? !!_.find(firstPOJO, (plainObject, index) => {
+              return !_.isEqual(plainObject, secondPOJO[index]);
+          })
+        : true;
+}
