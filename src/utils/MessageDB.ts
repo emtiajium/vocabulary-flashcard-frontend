0636c2d67d0a9a44e3267e59a127688c52e57e50
emtiajium <271emtiaj@gmail.com>
2021-07-15 08:40:26 +0600

    removed useless API execution

--- src/utils/MessageDB.ts
+++ src/utils/MessageDB.ts
@@ -1,5 +1,6 @@
 const MessageDB = {
     networkError: `Looks like something is wrong with your network. Please try later.`,
+    genericError: `Looks like something is wrong. Please try later.`,
 };
 
 export default MessageDB;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,6 @@ import Spinner from '@/views/Spinner.vue';
 import NativeStorage from '@/utils/NativeStorage';
 import { useBackButton, useIonRouter } from '@ionic/vue';
 import { App } from '@capacitor/app';
-import HttpHandler from '@/utils/HttpHandler';
-import User from '@/domains/User';
 import Toast from '@/utils/Toast';
 import MessageDB from '@/utils/MessageDB';
 
@@ -30,16 +28,12 @@ export default defineComponent({
         try {
             const localUser = await NativeStorage.getAuthorizedUser();
             if (localUser) {
-                // get the user info from the server
-                // as cohort can be updated anytime by the super admin
-                const user = await HttpHandler.get<User>(`/v1/users/self`);
-                await NativeStorage.resetAuthorizedUser({ ...user, jwToken: localUser.jwToken });
                 await this.$router.push('/authenticated-home');
             } else {
                 await this.$router.push('/sign-in');
             }
         } catch (error) {
-            await Promise.all([Toast.present(MessageDB.networkError), this.$router.push('/sign-in')]);
+            await Promise.all([Toast.present(MessageDB.genericError), this.$router.push('/sign-in')]);
         }
     },
 });
