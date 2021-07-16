a209187c96256db3295326366fcc2dcbd8c682c2
emtiajium <271emtiaj@gmail.com>
2021-07-16 15:26:21 +0600

    as structure has been changed to mimic the android folder

--- replace-generated-android-files.sh
+++ replace-generated-android-files.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
-cp ./android-replacer/AndroidManifest.xml ./android/app/src/main/AndroidManifest.xml
-cp ./android-replacer/ic_launcher_background.xml ./android/app/src/main/res/values/ic_launcher_background.xml
-cp ./android-replacer/strings.xml ./android/app/src/main/res/values/strings.xml
-cp ./android-replacer/styles.xml ./android/app/src/main/res/values/styles.xml
-cp ./android-replacer/variables.gradle ./android/variables.gradle
-rm -rf ./android/app/src/main/res/values-v26 && mkdir ./android/app/src/main/res/values-v26
-cp ./android-replacer/styles-v26.xml ./android/app/src/main/res/values-v26/styles.xml
 # Splash screen
-cp -R ./android-replacer/drawable ./android/app/src/main/res
-cp -R ./android-replacer/drawable-land-hdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-land-mdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-land-xhdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-land-xxhdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-land-xxxhdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-port-hdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-port-mdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-port-xhdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-port-xxhdpi ./android/app/src/main/res
-cp -R ./android-replacer/drawable-port-xxxhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-land-hdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-land-mdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-land-xhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-land-xxhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-land-xxxhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-port-hdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-port-mdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-port-xhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-port-xxhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/drawable-port-xxxhdpi ./android/app/src/main/res
 # Logo
-cp -R ./android-replacer/mipmap-hdpi ./android/app/src/main/res
-cp -R ./android-replacer/mipmap-mdpi ./android/app/src/main/res
-cp -R ./android-replacer/mipmap-xhdpi ./android/app/src/main/res
-cp -R ./android-replacer/mipmap-xxhdpi ./android/app/src/main/res
-cp -R ./android-replacer/mipmap-xxxhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/mipmap-hdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/mipmap-mdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/mipmap-xhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/mipmap-xxhdpi ./android/app/src/main/res
+cp -R ./android-replacer/app/src/main/res/mipmap-xxxhdpi ./android/app/src/main/res
+# Values
+cp ./android-replacer/app/src/main/res/values/ic_launcher_background.xml ./android/app/src/main/res/values/ic_launcher_background.xml
+cp ./android-replacer/app/src/main/res/values/strings.xml ./android/app/src/main/res/values/strings.xml
+cp ./android-replacer/app/src/main/res/values/styles.xml ./android/app/src/main/res/values/styles.xml
+rm -rf ./android/app/src/main/res/values-v26 && mkdir ./android/app/src/main/res/values-v26
+cp ./android-replacer/app/src/main/res/values-v26/styles-v26.xml ./android/app/src/main/res/values-v26/styles.xml
+# AndroidManifest
+cp ./android-replacer/app/src/main/AndroidManifest.xml ./android/app/src/main/AndroidManifest.xml
+# App-level build.gradle
+cp ./android-replacer/app/build.gradle ./android/app/build.gradle
+# Firebase
+cp ./android-replacer/app/google-services.json ./android/app/google-services.json
+
+# Project-level build.gradle
+cp ./android-replacer/android/build.gradle ./android/build.gradle
+cp ./android-replacer/android/variables.gradle ./android/variables.gradle
