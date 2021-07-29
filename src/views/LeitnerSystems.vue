5f9047cf1c87c43712b8d6027a68209b293b6d90
emtiajium <271emtiaj@gmail.com>
2021-07-29 21:30:07 +0600

    initial template

--- src/views/LeitnerSystems.vue
+++ src/views/LeitnerSystems.vue
@@ -1,13 +1,87 @@
 <template>
-    <div>hello!</div>
+    <ion-page>
+        <firecracker-header header-title="Leitner Boxes" content-id="leitner-boxes" menu-id="leitner-boxes-menu" />
+        <ion-content :fullscreen="true" id="leitner-boxes">
+            <ion-grid>
+                <ion-row>
+                    <ion-col size="6">
+                        <ion-card :button="true">
+                            <ion-card-content class="display-flex ion-justify-content-center ion-padding">
+                                <font-awesome-icon :icon="faBoxOpen" class="opened-box-icon" />
+                            </ion-card-content>
+                        </ion-card>
+                    </ion-col>
+                    <ion-col size="6">
+                        <ion-card :button="true">
+                            <ion-card-content class="display-flex ion-justify-content-center ion-padding">
+                                <font-awesome-icon :icon="faBoxOpen" class="opened-box-icon" />
+                            </ion-card-content>
+                        </ion-card>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col size="6">
+                        <ion-card :button="true">
+                            <ion-card-content class="display-flex ion-justify-content-center ion-padding">
+                                <font-awesome-icon :icon="faBoxOpen" class="opened-box-icon" />
+                            </ion-card-content>
+                        </ion-card>
+                    </ion-col>
+                    <ion-col size="6">
+                        <ion-card :button="true">
+                            <ion-card-content class="display-flex ion-justify-content-center ion-padding">
+                                <font-awesome-icon :icon="faBoxOpen" class="opened-box-icon" />
+                            </ion-card-content>
+                        </ion-card>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col size="4" />
+                    <ion-col size="4">
+                        <ion-card :button="true">
+                            <ion-card-content class="display-flex ion-justify-content-center ion-padding">
+                                <font-awesome-icon :icon="faBoxOpen" class="opened-box-icon" />
+                            </ion-card-content>
+                        </ion-card>
+                    </ion-col>
+                    <ion-col size="4" />
+                </ion-row>
+            </ion-grid>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { IonContent, IonPage, IonCard, IonCardContent, IonGrid, IonRow, IonCol } from '@ionic/vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faBoxOpen } from '@fortawesome/free-solid-svg-icons';
+import FirecrackerHeader from '@/views/FirecrackerHeader.vue';
 
 export default defineComponent({
     name: 'LeitnerSystems',
+    components: {
+        FirecrackerHeader,
+        IonContent,
+        IonPage,
+        FontAwesomeIcon,
+        IonCard,
+        IonCardContent,
+        IonGrid,
+        IonRow,
+        IonCol,
+    },
+    data() {
+        return {
+            faBoxOpen,
+        };
+    },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.opened-box-icon {
+    font-size: 60pt;
+    color: var(--ion-color-primary);
+}
+</style>
