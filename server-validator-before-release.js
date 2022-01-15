c1931f4c12c9df2a782ad7c080b86ef4681d63a1
emtiajium <271emtiaj@gmail.com>
2022-01-16 02:19:02 +0600

    added checking for HTTPS

--- server-validator-before-release.js
+++ server-validator-before-release.js
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/ban-ts-ignore */
 const Config = require('./config.json');
 
-if (Config.server.apiPrefix.search('elasticbeanstalk') === -1) {
-    console.log(`Current Config.server.apiPrefix is ${Config.server.apiPrefix}. It needs to be changed`);
+if (Config.server.apiPrefix.search('https') === -1 || Config.server.apiPrefix.search('elasticbeanstalk') === -1) {
+    console.error(`Current Config.server.apiPrefix is ${Config.server.apiPrefix}. It needs to be changed`);
     // @ts-ignore
     process.exit(1);
 }
