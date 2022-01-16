d9f51e0722a1432d0461246f9a9e155ceb245e3f
emtiajium <271emtiaj@gmail.com>
2022-01-16 15:56:47 +0600

    validated server

--- server-validator-before-release.js
+++ server-validator-before-release.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/ban-ts-ignore */
 const Config = require('./config.json');
 
-if (Config.server.apiPrefix.search('https') === -1 || Config.server.apiPrefix.search('elasticbeanstalk') === -1) {
+if (Config.server.apiPrefix.search('https://api.firecrackervocabulary.com/') === -1) {
     console.error(`Current Config.server.apiPrefix is ${Config.server.apiPrefix}. It needs to be changed`);
     // @ts-ignore
     process.exit(1);
