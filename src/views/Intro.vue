89eab7f4617ccbe1fb4889edb01438e458f4088e
emtiajium <271emtiaj@gmail.com>
2022-01-30 18:52:57 +0600

    tried to give a better UI experience

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,26 +1,39 @@
 <template>
-    <view>
-        <ion-card class="ion-padding">
-            Ever wonder why you still remember the meaning of the words you learned in your childhood but struggle for a
-            vocab learned a few days or weeks back?
-        </ion-card>
-        <ion-card class="ion-padding">
-            According to science, spaced repetition is one of the techniques to retain anything in the long term. The
-            Leitner system works based on this principle. It suggests creating a <strong>flashcard</strong> for each
-            item and reviewing those at an interval.
-        </ion-card>
-        <ion-card class="ion-padding"> Join us to embed newly introduced words in your long-term memory. </ion-card>
-    </view>
+    <ion-grid>
+        <ion-row class="display-flex ion-justify-content-center">
+            <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
+                <ion-card class="ion-padding">
+                    Ever wonder why you still remember the meaning of the words you learned in your childhood but
+                    struggle for a vocab learned a few days or weeks back?
+                </ion-card>
+            </ion-col>
+            <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
+                <ion-card class="ion-padding">
+                    According to science, spaced repetition is one of the techniques to retain anything in the long
+                    term. The Leitner system works based on this principle. It suggests creating a
+                    <strong>flashcard</strong> for each item and reviewing those at an interval.
+                </ion-card>
+            </ion-col>
+            <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
+                <ion-card class="ion-padding">
+                    Join us to embed newly introduced words in your long-term memory.
+                </ion-card>
+            </ion-col>
+        </ion-row>
+    </ion-grid>
 </template>
 
 <script lang="ts">
-import { IonCard } from '@ionic/vue';
+import { IonCard, IonCol, IonRow, IonGrid } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'Intro',
     components: {
         IonCard,
+        IonCol,
+        IonRow,
+        IonGrid,
     },
 });
 </script>
