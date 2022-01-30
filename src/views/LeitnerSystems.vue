e4343186009ff4dc2033c378ecae9f9180272136
emtiajium <271emtiaj@gmail.com>
2022-01-30 23:47:07 +0600

    fixed margin issue

--- src/views/LeitnerSystems.vue
+++ src/views/LeitnerSystems.vue
@@ -2,64 +2,58 @@
     <ion-page>
         <firecracker-header header-title="My Flashcards" content-id="leitner-boxes" menu-id="leitner-boxes-menu" />
         <ion-content :fullscreen="true" id="leitner-boxes">
-            <ion-grid>
-                <ion-row class="display-flex ion-justify-content-center">
-                    <ion-col sizeXs="12" sizeSm="12" sizeMd="10" sizeLg="10" sizeXl="10">
-                        <ion-card :button="true" class="description-margin" @click="toggle">
-                            <ion-card-content>
-                                <ion-card-subtitle>
-                                    People often struggle to memorize vocabulary in the long run. Fortunately, there are
-                                    several methods available to overcome this difficulty. One of these is Leitner
-                                    System.
-                                </ion-card-subtitle>
-                                <ion-card-subtitle v-show="seeLess" class="see-less"> ... See More </ion-card-subtitle>
-                                <ion-card-subtitle v-show="!seeLess" class="see-more ion-padding-top">
-                                    Sebastian Leitner, a German journalist, proposed this method based on the principle
-                                    of spaced repetition in which items can be repetitively reviewed until the learner
-                                    gets fully satisfied.
-                                </ion-card-subtitle>
-                                <leitner-systems-flow v-show="!seeLess" class="see-more" />
-                                <ion-card-subtitle v-show="!seeLess" class="see-more">
-                                    In this method, there are several boxes containing flashcards. If you wish to
-                                    remember a newly introduced vocab, move the word into the first box. Review it the
-                                    next day, either move it to the next one if you remember the meaning or keep it
-                                    within the box. In this way, review the items in the second box. Move it back to the
-                                    previous box if you forget the definitions, but forward it to the next box if
-                                    everything is okay. As you have already read the same vocabulary for two days, rest
-                                    for the next day. Review it on the fourth day and move it forward or backwards. Then
-                                    on the seventh day, review it again.
-                                </ion-card-subtitle>
-                            </ion-card-content>
-                        </ion-card>
-                    </ion-col>
-                </ion-row>
-            </ion-grid>
-            <ion-grid>
-                <ion-row class="display-flex ion-justify-content-center">
-                    <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
-                        <leitner-single-box :box="1" :count="count[1]" />
-                    </ion-col>
-                    <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
-                        <leitner-single-box :box="2" :count="count[2]" />
-                    </ion-col>
-                    <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
-                        <leitner-single-box :box="3" :count="count[3]" />
-                    </ion-col>
-                    <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
-                        <leitner-single-box :box="4" :count="count[4]" />
-                    </ion-col>
-                    <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
-                        <leitner-single-box :box="5" :count="count[5]" />
-                    </ion-col>
-                </ion-row>
-            </ion-grid>
+            <ion-row class="display-flex ion-justify-content-center">
+                <ion-col sizeXs="12" sizeSm="12" sizeMd="10" sizeLg="10" sizeXl="10" class="description-container">
+                    <ion-card :button="true" class="description-margin" @click="toggle">
+                        <ion-card-content>
+                            <ion-card-subtitle>
+                                People often struggle to memorize vocabulary in the long run. Fortunately, there are
+                                several methods available to overcome this difficulty. One of these is Leitner System.
+                            </ion-card-subtitle>
+                            <ion-card-subtitle v-show="seeLess" class="see-less"> ... See More </ion-card-subtitle>
+                            <ion-card-subtitle v-show="!seeLess" class="see-more ion-padding-top">
+                                Sebastian Leitner, a German journalist, proposed this method based on the principle of
+                                spaced repetition in which items can be repetitively reviewed until the learner gets
+                                fully satisfied.
+                            </ion-card-subtitle>
+                            <leitner-systems-flow v-show="!seeLess" class="see-more" />
+                            <ion-card-subtitle v-show="!seeLess" class="see-more">
+                                In this method, there are several boxes containing flashcards. If you wish to remember a
+                                newly introduced vocab, move the word into the first box. Review it the next day, either
+                                move it to the next one if you remember the meaning or keep it within the box. In this
+                                way, review the items in the second box. Move it back to the previous box if you forget
+                                the definitions, but forward it to the next box if everything is okay. As you have
+                                already read the same vocabulary for two days, rest for the next day. Review it on the
+                                fourth day and move it forward or backwards. Then on the seventh day, review it again.
+                            </ion-card-subtitle>
+                        </ion-card-content>
+                    </ion-card>
+                </ion-col>
+            </ion-row>
+            <ion-row class="display-flex ion-justify-content-center">
+                <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
+                    <leitner-single-box :box="1" :count="count[1]" />
+                </ion-col>
+                <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
+                    <leitner-single-box :box="2" :count="count[2]" />
+                </ion-col>
+                <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
+                    <leitner-single-box :box="3" :count="count[3]" />
+                </ion-col>
+                <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
+                    <leitner-single-box :box="4" :count="count[4]" />
+                </ion-col>
+                <ion-col sizeXs="6" sizeSm="4" sizeMd="3" sizeLg="3" sizeXl="2">
+                    <leitner-single-box :box="5" :count="count[5]" />
+                </ion-col>
+            </ion-row>
         </ion-content>
     </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonContent, IonPage, IonGrid, IonRow, IonCol, IonCardSubtitle, IonCardContent, IonCard } from '@ionic/vue';
+import { IonContent, IonPage, IonRow, IonCol, IonCardSubtitle, IonCardContent, IonCard } from '@ionic/vue';
 import FirecrackerHeader from '@/views/FirecrackerHeader.vue';
 import LeitnerSingleBox from '@/views/LeitnerSingleBox.vue';
 import LeitnerSystemsFlow from '@/views/LeitnerSystemsFlow.vue';
@@ -73,7 +67,6 @@ export default defineComponent({
         LeitnerSystemsFlow,
         IonContent,
         IonPage,
-        IonGrid,
         IonRow,
         IonCol,
         IonCard,
@@ -120,6 +113,12 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.description-container {
+    padding-inline-start: unset;
+    padding-inline-end: unset;
+    padding-top: unset;
+    padding-bottom: unset;
+}
 .description-margin {
     margin: unset;
     margin-inline: unset;
