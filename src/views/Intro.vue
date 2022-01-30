b8e65a1cfcc62eef6931af00b7e33794980e541b
emtiajium <271emtiaj@gmail.com>
2022-01-30 22:57:23 +0600

    removed useless ion-grid

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,30 +1,28 @@
 <template>
-    <ion-grid>
-        <ion-row class="display-flex ion-justify-content-center">
-            <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
-                <ion-card class="ion-padding">
-                    Ever wonder why you still remember the meaning of the words you learned in your childhood but
-                    struggle for a vocab learned a few days or weeks back?
-                </ion-card>
-            </ion-col>
-            <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
-                <ion-card class="ion-padding">
-                    According to science, spaced repetition is one of the techniques to retain anything in the long
-                    term. The Leitner system works based on this principle. It suggests creating a
-                    <strong>flashcard</strong> for each item and reviewing those at an interval.
-                </ion-card>
-            </ion-col>
-            <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
-                <ion-card class="ion-padding">
-                    Join us to embed newly introduced words in your long-term memory.
-                </ion-card>
-            </ion-col>
-        </ion-row>
-    </ion-grid>
+    <ion-row class="display-flex ion-justify-content-center">
+        <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
+            <ion-card class="no-margin ion-padding">
+                Ever wonder why you still remember the meaning of the words you learned in your childhood but struggle
+                for a vocab learned a few days or weeks back?
+            </ion-card>
+        </ion-col>
+        <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
+            <ion-card class="no-margin ion-padding">
+                According to science, spaced repetition is one of the techniques to retain anything in the long term.
+                The Leitner system works based on this principle. It suggests creating a
+                <strong>flashcard</strong> for each item and reviewing those at an interval.
+            </ion-card>
+        </ion-col>
+        <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
+            <ion-card class="no-margin ion-padding">
+                Join us to embed newly introduced words in your long-term memory.
+            </ion-card>
+        </ion-col>
+    </ion-row>
 </template>
 
 <script lang="ts">
-import { IonCard, IonCol, IonRow, IonGrid } from '@ionic/vue';
+import { IonCard, IonCol, IonRow } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -33,7 +31,6 @@ export default defineComponent({
         IonCard,
         IonCol,
         IonRow,
-        IonGrid,
     },
 });
 </script>
