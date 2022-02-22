4eaadf6c68574e78972c14f9c132b66718b688a2
emtiajium <271emtiaj@gmail.com>
2022-02-22 14:27:25 +0600

    gradual color changing functionality for the box

--- src/views/LeitnerSingleBox.vue
+++ src/views/LeitnerSingleBox.vue
@@ -1,43 +1,51 @@
 <template>
     <ion-card :button="true" class="transparent margin-top-bottom-unset" @click="navigate">
         <ion-card-content class="no-padding">
-            <ion-grid>
-                <ion-row class="display-flex ion-justify-content-center">
-                    <img src="/assets/icon/empty-box.svg" alt="Leitner Box" class="opened-box-icon" />
-                </ion-row>
-                <ion-row class="display-flex ion-justify-content-center">
-                    <ion-card-subtitle> {{ mappedBoxWithDays[`BOX_${box}`] }} Box </ion-card-subtitle>
-                </ion-row>
-                <ion-row class="display-flex ion-justify-content-center">
-                    <spinner v-if="count === -1" />
-                    <ion-card-subtitle v-if="count !== -1"> {{ count }} Vocab </ion-card-subtitle>
-                </ion-row>
-            </ion-grid>
+            <ion-row class="display-flex ion-justify-content-center opened-box-icon">
+                <empty-box :color="colors[box]"></empty-box>
+            </ion-row>
+            <ion-row class="display-flex ion-justify-content-center">
+                <ion-card-subtitle> {{ mappedBoxWithDays[`BOX_${box}`] }} Box </ion-card-subtitle>
+            </ion-row>
+            <ion-row class="display-flex ion-justify-content-center">
+                <spinner v-if="count === -1" />
+                <ion-card-subtitle v-if="count !== -1"> {{ count }} Vocab </ion-card-subtitle>
+            </ion-row>
         </ion-card-content>
     </ion-card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonCard, IonCardContent, IonCardSubtitle, IonGrid, IonRow } from '@ionic/vue';
+import { IonCard, IonCardContent, IonCardSubtitle, IonRow } from '@ionic/vue';
 import MappedLeitnerBoxWithDays from '@/domains/MappedLeitnerBoxWithDays';
 import Spinner from '@/views/Spinner.vue';
 import NativeStorage from '@/utils/NativeStorage';
+import EmptyBox from '@/views/EmptyBox.vue';
 
 export default defineComponent({
     name: 'LeitnerSingleBox',
     components: {
+        EmptyBox,
         Spinner,
         IonCard,
         IonCardContent,
         IonCardSubtitle,
-        IonGrid,
         IonRow,
     },
     props: ['box', 'count'],
     data() {
         return {
             mappedBoxWithDays: MappedLeitnerBoxWithDays,
+            // generated color using
+            // https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+            colors: {
+                '1': '#c7e5c5',
+                '2': '#7fc47a',
+                '3': '#49ac41',
+                '4': '#348b2e',
+                '5': '#125b11',
+            },
         };
     },
     methods: {
@@ -50,16 +58,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.opened-box-icon {
-    max-width: 65%;
-}
-
-@media only screen and (min-device-width: 480px) {
-    .opened-box-icon {
-        width: 40%;
-    }
-}
-
 .no-padding {
     padding-top: unset;
     padding-bottom: unset;
