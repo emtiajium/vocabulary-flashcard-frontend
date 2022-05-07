c290ee2ae589a30db33e71f3b976c823f3146d42
emtiajium <271emtiaj@gmail.com>
2022-05-07 21:10:44 +0600

    organized the css

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,9 @@
 import 'reflect-metadata';
+import '@/styles/main.css';
 import { createApp } from 'vue';
 import { IonicVue } from '@ionic/vue';
 import App from '@/App.vue';
 import router from '@/router';
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/vue/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/vue/css/normalize.css';
-import '@ionic/vue/css/structure.css';
-import '@ionic/vue/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
-
-/* Theme variables */
-import '@/theme/variables.css';
-
-/* All custom/imported styling */
-import '@/theme/fonts.css';
-import '@/theme/material-icons.css';
-import '@/theme/custom-styles.css';
-
 import { initTheme } from '@/utils/dark-mode';
 
 const app = createApp(App).use(IonicVue).use(router);
