b4784bec8f27f8e1751368666c91218b8c727097
emtiajium <271emtiaj@gmail.com>
2022-05-29 19:22:40 +0600

    wrapped within the card-subtitle component

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -2,27 +2,33 @@
     <ion-row class="display-flex ion-justify-content-center">
         <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
             <ion-card class="margin-top-bottom-unset ion-padding">
-                Ever wonder why you still remember the meaning of the words you learned in your childhood but struggle
-                with a vocab learned a few days or weeks back?
+                <ion-card-subtitle>
+                    Ever wonder why you still remember the meaning of the words you learned in your childhood but
+                    struggle with a vocab learned a few days or weeks back?
+                </ion-card-subtitle>
             </ion-card>
         </ion-col>
         <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
             <ion-card class="margin-top-bottom-unset ion-padding">
-                According to science, <strong>spaced repetition</strong> is one of the techniques to retain anything in
-                the long term. The Leitner system works based on this principle. It suggests creating a
-                <strong>flashcard</strong> for each item and reviewing those at an interval.
+                <ion-card-subtitle>
+                    According to science, <strong>spaced repetition</strong> is one of the techniques to retain anything
+                    in the long term. The Leitner system works based on this principle. It suggests creating a
+                    <strong>flashcard</strong> for each item and reviewing those at an interval.
+                </ion-card-subtitle>
             </ion-card>
         </ion-col>
         <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
             <ion-card class="margin-top-bottom-unset ion-padding">
-                Join us to embed newly introduced words in your long-term memory.
+                <ion-card-subtitle>
+                    Join us to embed newly introduced words in your long-term memory.
+                </ion-card-subtitle>
             </ion-card>
         </ion-col>
     </ion-row>
 </template>
 
 <script lang="ts">
-import { IonCard, IonCol, IonRow } from '@ionic/vue';
+import { IonCard, IonCol, IonRow, IonCardSubtitle } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -31,6 +37,7 @@ export default defineComponent({
         IonCard,
         IonCol,
         IonRow,
+        IonCardSubtitle,
     },
 });
 </script>
