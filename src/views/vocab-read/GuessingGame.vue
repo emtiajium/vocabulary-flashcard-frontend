b051b61ba286879b75cf70e0e65ffb1a637985b6
Emtiaj Hasan <271emtiaj@gmail.com>
2024-05-14 21:00:08 +0600

    data from the server

--- src/views/vocab-read/GuessingGame.vue
+++ src/views/vocab-read/GuessingGame.vue
@@ -7,22 +7,25 @@
             type="MENU"
         />
         <ion-content :fullscreen="true" id="guessing-game">
-            <ion-row class="display-flex ion-justify-content-center">
-                <ion-col sizeXs="12" sizeSm="12" sizeMd="10" sizeLg="8" sizeXl="8">
-                    <div class="container">
-                        <div id="question">
-                            {{ vocabularies[currentQuestionIndex].meaning }}
-                        </div>
-                        <div id="options">
-                            <ion-textarea
-                                placeholder="Guess the word"
-                                @ionChange="givenAnswer = $event.target.value"
-                            ></ion-textarea>
-                        </div>
-                        <div id="result">{{ resultMessage }}</div>
-                        <div class="display-flex ion-justify-content-around">
-                            <button @click="checkAnswer()">Check</button>
-                            <button id="next-btn" @click="onClickNextButton">Next</button>
+            <ion-row class="display-flex ion-justify-content-center ion-padding-top">
+                <ion-col sizeXs="12" sizeSm="12" sizeMd="8" sizeLg="6" sizeXl="6">
+                    <spinner v-if="isLoading" />
+                    <div v-if="!isLoading && vocabularies.length">
+                        <ion-card-subtitle class="ion-text-center ion-padding-bottom">
+                            {{ vocabularies[currentVocabularyIndex].meaning }}
+                        </ion-card-subtitle>
+                        <ion-textarea
+                            placeholder="Guess the word"
+                            inputmode="text"
+                            :value="givenAnswer"
+                            @ionChange="givenAnswer = $event.target.value"
+                        />
+                        <ion-card-subtitle class="ion-padding-top ion-text-end">
+                            {{ resultMessage }}
+                        </ion-card-subtitle>
+                        <div class="display-flex ion-justify-content-end ion-padding-top">
+                            <ion-button @click="checkAnswer()">Check</ion-button>
+                            <ion-button @click="onClickNextButton()">Next</ion-button>
                         </div>
                     </div>
                 </ion-col>
@@ -33,8 +36,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonContent, IonRow, IonCol, IonTextarea } from '@ionic/vue';
+import { IonPage, IonContent, IonRow, IonCol, IonTextarea, IonButton, IonCardSubtitle } from '@ionic/vue';
 import FirecrackerHeader from '@/views/shared/FirecrackerHeader.vue';
+import Spinner from '@/views/shared/Spinner.vue';
+import HttpHandler from '@/utils/HttpHandler';
+import Toast from '@/utils/Toast';
+import MessageDB from '@/utils/MessageDB';
+import { RandomlyChosenMeaningResponse } from '@/domains/RandomlyChosenMeaningResponse';
 
 export default defineComponent({
     name: 'GuessingGame',
@@ -45,49 +53,60 @@ export default defineComponent({
         IonRow,
         IonCol,
         IonTextarea,
+        Spinner,
+        IonButton,
+        IonCardSubtitle,
     },
     data() {
         return {
-            vocabularies: [
-                { meaning: 'to make clear or easier to understand', word: 'clarify' },
-                { meaning: 'a spoken or written account of connected events', word: 'narrative' },
-                { meaning: 'a person or thing that represents an idea or quality', word: 'symbol' },
-                { meaning: 'to give or hand over (something) to someone else', word: 'delegate' },
-                { meaning: 'existing at the same time or happening at the same time', word: 'simultaneous' },
-            ],
-            currentQuestionIndex: 0,
-            resultMessage: '',
             correctSound: new Audio('/assets/audio-clips/correct-answer.mp3'),
             incorrectSound: new Audio('/assets/audio-clips/wrong-answer.mp3'),
+            isLoading: true,
+            vocabularies: [] as RandomlyChosenMeaningResponse[],
+            currentVocabularyIndex: 0,
             givenAnswer: '',
+            resultMessage: '',
         };
     },
     async ionViewDidEnter() {
-        console.log('here!');
+        await this.loadVocabularies();
     },
     methods: {
+        async loadVocabularies(): Promise<void> {
+            try {
+                this.vocabularies = await HttpHandler.get<RandomlyChosenMeaningResponse[]>(
+                    `/v1/vocabularies/definitions/random-search`,
+                );
+            } catch {
+                await Toast.present(MessageDB.genericError);
+            } finally {
+                this.isLoading = false;
+            }
+        },
+
         checkAnswer(): void {
             const correctVocabulary = this.vocabularies.find(
-                (item) => item.word === this.vocabularies[this.currentQuestionIndex].word,
+                (vocabulary) => vocabulary.word === this.vocabularies[this.currentVocabularyIndex].word,
             );
 
             if (this.givenAnswer.trim().toLowerCase() === correctVocabulary?.word.trim().toLowerCase()) {
                 this.resultMessage = 'Correct!';
                 this.playSound(this.correctSound);
             } else {
-                this.resultMessage = `Incorrect. The correct answer is "${
-                    this.vocabularies[this.currentQuestionIndex].word
+                this.resultMessage = `Incorrect. The correct word is "${
+                    this.vocabularies[this.currentVocabularyIndex].word
                 }".`;
                 this.playSound(this.incorrectSound);
             }
         },
 
         onClickNextButton(): void {
-            this.currentQuestionIndex += 1;
-            if (this.currentQuestionIndex >= this.vocabularies.length) {
-                this.currentQuestionIndex = 0;
+            this.currentVocabularyIndex += 1;
+            if (this.currentVocabularyIndex >= this.vocabularies.length) {
+                this.currentVocabularyIndex = 0;
             }
             this.resultMessage = '';
+            this.givenAnswer = '';
         },
 
         playSound(sound: HTMLAudioElement): void {
@@ -100,50 +119,4 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
-.container {
-    max-width: 600px;
-    margin: 0 auto;
-}
-
-h1 {
-    margin-bottom: 30px;
-}
-
-#options {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    margin-bottom: 20px;
-}
-
-.option {
-    margin: 5px;
-    padding: 10px 20px;
-    border: 2px solid #333;
-    cursor: pointer;
-    transition: background-color 0.3s;
-}
-
-.option:hover {
-    background-color: #f0f0f0;
-}
-
-#next-btn {
-    margin-top: 20px;
-    padding: 10px 20px;
-    background-color: #333;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-    transition: background-color 0.3s;
-}
-
-#next-btn:hover {
-    background-color: #555;
-}
-
-#result {
-    margin-top: 20px;
-}
-</style>
+<style scoped></style>
