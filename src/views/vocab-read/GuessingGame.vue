43151ecc69a406925422f8cd3d41fd42438a3004
Emtiaj Hasan <271emtiaj@gmail.com>
2024-05-16 15:43:56 +0600

    implemented slider

--- src/views/vocab-read/GuessingGame.vue
+++ src/views/vocab-read/GuessingGame.vue
@@ -7,7 +7,7 @@
             type="MENU"
         />
         <ion-content :fullscreen="true" id="guessing-game">
-            <ion-row class="display-flex ion-justify-content-center ion-padding-top">
+            <ion-row class="display-flex ion-justify-content-center">
                 <ion-col sizeXs="12" sizeSm="10" sizeMd="8" sizeLg="6" sizeXl="6">
                     <spinner v-if="isLoading" />
 
@@ -17,28 +17,43 @@
                         </ion-card-subtitle>
                     </div>
 
-                    <div v-if="!isLoading && vocabularies.length" class="ion-padding-start ion-padding-end">
-                        <ion-card-subtitle class="ion-text-center ion-padding-bottom">
-                            {{ vocabularies[currentVocabularyIndex].meaning }}
-                        </ion-card-subtitle>
-                        <ion-textarea
-                            placeholder="Guess the word"
-                            inputmode="text"
-                            :auto-grow="true"
-                            :value="givenAnswer"
-                            @ionChange="givenAnswer = $event.target.value"
-                        />
-                        <ion-card-subtitle class="ion-padding-top ion-text-end">
-                            {{ resultMessage }}
-                        </ion-card-subtitle>
-                        <div class="display-flex ion-justify-content-end ion-padding-top">
-                            <ion-button aria-label="Skip and try next" @click="skip()"> Skip </ion-button>
-                            <ion-button aria-label="Check the answer" @click="checkAnswer()"> Check </ion-button>
-                            <ion-button aria-label="Back to the previous" @click="onClickPrevious()">
-                                Previous
-                            </ion-button>
-                            <ion-button aria-label="Navigate to the next" @click="onClickNext()"> Next </ion-button>
+                    <div class="swiper ion-margin-start ion-margin-end" v-show="!isLoading && vocabularies.length">
+                        <div class="swiper-wrapper">
+                            <div v-for="(vocabulary, index) of vocabularies" :key="index" class="swiper-slide">
+                                <div class="ion-padding ion-margin">
+                                    <ion-card-subtitle class="ion-text-center ion-padding-bottom">
+                                        {{ vocabulary.meaning }}
+                                    </ion-card-subtitle>
+                                    <div class="ion-padding-start ion-padding-end">
+                                        <ion-textarea
+                                            placeholder="Guess the word"
+                                            inputmode="text"
+                                            :auto-grow="true"
+                                            :value="givenAnswer"
+                                            @ionChange="givenAnswer = $event.target.value"
+                                        />
+                                    </div>
+                                    <ion-card-subtitle
+                                        class="ion-padding-top ion-padding-start ion-padding-end ion-text-center"
+                                    >
+                                        {{ resultMessage }}
+                                    </ion-card-subtitle>
+                                    <div
+                                        class="
+                                            display-flex
+                                            ion-justify-content-end ion-padding-top ion-padding-bottom ion-padding-end
+                                        "
+                                    >
+                                        <ion-button aria-label="Check the answer" @click="checkAnswer(vocabulary)">
+                                            Check
+                                        </ion-button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-pagination"></div>
                     </div>
                 </ion-col>
             </ion-row>
@@ -57,6 +72,8 @@ import MessageDB from '@/utils/MessageDB';
 import { RandomlyChosenMeaningResponse } from '@/domains/RandomlyChosenMeaningResponse';
 import { format } from 'date-fns';
 import NativeStorage from '@/utils/NativeStorage';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
 export default defineComponent({
     name: 'GuessingGame',
@@ -77,9 +94,9 @@ export default defineComponent({
             incorrectSound: new Audio('/assets/audio-clips/wrong-answer.mp3'),
             isLoading: true,
             vocabularies: [] as RandomlyChosenMeaningResponse[],
-            currentVocabularyIndex: 0,
             givenAnswer: '',
             resultMessage: '',
+            swiper: {} as Swiper,
         };
     },
     async ionViewDidEnter() {
@@ -87,18 +104,45 @@ export default defineComponent({
     },
     ionViewWillLeave() {
         this.clean();
+        this.swiper.destroy();
     },
     methods: {
         async init(): Promise<void> {
+            this.initSwiper();
             const cachedRandomlyChosenMeaningResponse = await NativeStorage.getGuessingGameVocabularies();
             const today = format(new Date(), 'yyyy-MM-dd');
             if (cachedRandomlyChosenMeaningResponse?.createdAt !== today) {
                 await NativeStorage.removeGuessingGameVocabularies().catch();
                 await this.loadVocabularies();
+                this.isLoading = false;
             } else {
                 this.vocabularies = cachedRandomlyChosenMeaningResponse.data;
                 this.isLoading = false;
             }
+            this.swiper.init();
+        },
+
+        initSwiper(): void {
+            this.swiper = new Swiper('.swiper', {
+                modules: [Navigation, Pagination],
+                init: false,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                    type: 'bullets',
+                    renderBullet: (index: number, className: string): string => {
+                        return `<span class="${className}">${index + 1}</span>`;
+                    },
+                },
+            });
+
+            this.swiper.on('slideChange', () => {
+                this.onChangeVocabulary();
+            });
         },
 
         async loadVocabularies(): Promise<void> {
@@ -114,47 +158,20 @@ export default defineComponent({
                 });
             } catch {
                 await Toast.present(MessageDB.genericError);
-            } finally {
-                this.isLoading = false;
             }
         },
 
-        checkAnswer(): void {
-            const correctVocabulary = this.vocabularies[this.currentVocabularyIndex];
+        checkAnswer(correctVocabulary: RandomlyChosenMeaningResponse): void {
             if (this.givenAnswer.trim().toLowerCase() === correctVocabulary.word.trim().toLowerCase()) {
                 this.resultMessage = 'Correct!';
                 this.playSound(this.correctSound);
             } else {
-                this.resultMessage = `Incorrect. The correct word is "${
-                    this.vocabularies[this.currentVocabularyIndex].word
-                }".`;
+                this.resultMessage = `Incorrect. The correct word is "${correctVocabulary.word}".`;
                 this.playSound(this.incorrectSound);
             }
         },
 
-        onClickNext(): void {
-            this.currentVocabularyIndex += 1;
-            if (this.currentVocabularyIndex >= this.vocabularies.length) {
-                this.currentVocabularyIndex = 0;
-            }
-            this.resultMessage = '';
-            this.givenAnswer = '';
-        },
-
-        skip(): void {
-            this.currentVocabularyIndex += 1;
-            if (this.currentVocabularyIndex >= this.vocabularies.length) {
-                this.currentVocabularyIndex = 0;
-            }
-            this.resultMessage = '';
-            this.givenAnswer = '';
-        },
-
-        onClickPrevious(): void {
-            this.currentVocabularyIndex -= 1;
-            if (this.currentVocabularyIndex < 0) {
-                this.currentVocabularyIndex = 0;
-            }
+        onChangeVocabulary(): void {
             this.resultMessage = '';
             this.givenAnswer = '';
         },
@@ -171,10 +188,22 @@ export default defineComponent({
             this.vocabularies = [];
             this.givenAnswer = '';
             this.resultMessage = '';
-            this.currentVocabularyIndex = 0;
         },
     },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.swiper {
+    width: 100%;
+    height: 100%;
+}
+
+.swiper-slide {
+    background: var(--ion-card-background);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: var(--default-border-radius);
+}
+</style>
