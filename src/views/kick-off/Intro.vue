99eecab0408b7bea1ed8dd7bfc75d12f79be7fb2
Emtiaj Hasan <271emtiaj@gmail.com>
2024-05-23 11:48:29 +0600

    re-designed home page

--- src/views/kick-off/Intro.vue
+++ src/views/kick-off/Intro.vue
@@ -1,55 +1,100 @@
 <template>
-    <ion-row class="display-flex ion-justify-content-center ion-align-items-center">
-        <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
-            <ion-card class="margin-top-bottom-unset ion-padding">
-                <ion-card-subtitle>
-                    Ever wonder why you still remember the meaning of the words you learned in your childhood but
-                    struggle with a vocab learned a few days or weeks back?
-                </ion-card-subtitle>
-            </ion-card>
-        </ion-col>
-        <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
-            <ion-card class="margin-top-bottom-unset ion-padding">
-                <ion-card-subtitle>
-                    According to science, <strong>spaced repetition</strong> is one of the techniques to retain anything
-                    in the long term. The Leitner system works based on this principle. It suggests creating a
-                    <strong>flashcard</strong> for each item and reviewing those at an interval.
-                </ion-card-subtitle>
-            </ion-card>
-        </ion-col>
-        <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
-            <ion-card class="margin-top-bottom-unset ion-padding">
-                <ion-card-subtitle>
-                    Join us to embed newly introduced words in your long-term memory.
-                </ion-card-subtitle>
-            </ion-card>
-        </ion-col>
+    <ion-card v-if="currentIndex === 0" class="margin-top-bottom-unset">
+        <div class="display-flex ion-justify-content-center">
+            <thinking-icon
+                :fill-color-variable="'--ion-color-primary'"
+                :stroke-color-variable="'--ion-color-primary'"
+                class="icon-container"
+            />
+        </div>
+        <ion-card-title class="ion-padding-top ion-text-center">
+            Ever wonder why you still remember the meaning of the words you learned in your childhood but struggle with
+            a vocab learned a few days or weeks back?
+        </ion-card-title>
+    </ion-card>
 
-        <ion-col sizeXs="12" sizeSm="12" sizeMd="4" sizeLg="3" sizeXl="2">
-            <ion-card class="margin-top-bottom-unset ion-padding">
-                <ion-card-subtitle>
-                    Don't miss the <strong>word guessing game</strong> to challenge yourself to think critically and
-                    make connections between words and meanings to enhance your cognitive abilities. It's a fun and
-                    engaging way to learn.
-                </ion-card-subtitle>
-            </ion-card>
-        </ion-col>
-    </ion-row>
+    <ion-card v-if="currentIndex === 1" class="margin-top-bottom-unset">
+        <div class="display-flex ion-justify-content-center">
+            <box-icon
+                :fill-color-variable="'--ion-color-primary'"
+                :stroke-color-variable="'--ion-color-primary'"
+                class="icon-container"
+            />
+        </div>
+        <ion-card-title class="ion-padding-top ion-text-center">
+            According to science, <strong>spaced repetition</strong> is one of the techniques to retain anything in the
+            long term. The Leitner system works based on this principle. It suggests creating a
+            <strong>flashcard</strong> for each item and reviewing those at an interval.
+        </ion-card-title>
+    </ion-card>
+
+    <ion-card v-if="currentIndex === 2" class="margin-top-bottom-unset">
+        <div class="display-flex ion-justify-content-center">
+            <join-us-icon
+                :fill-color-variable="'--ion-color-primary'"
+                :stroke-color-variable="'--ion-color-primary'"
+                class="icon-container"
+            />
+        </div>
+        <ion-card-title class="ion-padding-top ion-text-center">
+            Join us to embed newly introduced words in your long-term memory.
+        </ion-card-title>
+    </ion-card>
+
+    <ion-card v-if="currentIndex === 3" class="margin-top-bottom-unset">
+        <div class="display-flex ion-justify-content-center">
+            <fun-icon
+                :fill-color-variable="'--ion-color-primary'"
+                :stroke-color-variable="'--ion-color-primary'"
+                class="icon-container"
+            />
+        </div>
+        <ion-card-title class="ion-padding-top ion-text-center">
+            Don't miss the <strong>word guessing game</strong> to challenge yourself to think critically and make
+            connections between words and meanings to enhance your cognitive abilities. It's a fun and engaging way to
+            learn.
+        </ion-card-title>
+    </ion-card>
 </template>
 
 <script lang="ts">
-import { IonCard, IonCol, IonRow, IonCardSubtitle } from '@ionic/vue';
+import { IonCard, IonCardTitle } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import ThinkingIcon from '@/media/ThinkingIcon.vue';
+import JoinUsIcon from '@/media/JoinUsIcon.vue';
+import FunIcon from '@/media/FunIcon.vue';
+import BoxIcon from '@/media/BoxIcon.vue';
 
 export default defineComponent({
     name: 'Intro',
     components: {
+        BoxIcon,
+        FunIcon,
+        JoinUsIcon,
+        ThinkingIcon,
         IonCard,
-        IonCol,
-        IonRow,
-        IonCardSubtitle,
+        IonCardTitle,
     },
+    props: ['currentIndex'],
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.icon-container {
+    width: 50%;
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+    .icon-container {
+        width: 40%;
+    }
+}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+    .icon-container {
+        width: 30%;
+    }
+}
+</style>
